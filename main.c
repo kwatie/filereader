cb0d86e4a1bd21249e86ef208c3f3ec8e9d80c47
kwatie <32926109+kwatie@users.noreply.github.com>
2017-10-20 15:11:31 +1100

    Inserted comments explaining code

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+/**
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -8,38 +13,80 @@ int main(int argc, char *argv[]) {
     short cHead = 0;
     short Obj = 0;
     short Make = 0;
-    short Other = -1;
-    char* point;
+    short Other = 0;
+    char *point;
+//    char *cFile = strrchr(argv, '.');
+//    char *hFile = 'h';
+//    char *oFile = 'o';
 
-        for (int i = 0; i < argc; i++) {
-            point = strrchr(argv[i], '.');
+
+//Loop to iterate through arguments
+    for (int i = 0; i < argc; i++) {
+
+        //Points the pointer to the last occurrence of a '.' in the argument
+        point = strrchr(argv[i], (const char) '.');
+
+        //Quick error check
+        if(point == !NULL) {
+            printf("Pointer has been set\n");
+        }
+
+        //If the pointer fails to find a '.' in the argument, check for makefile
+        if(point == NULL) {
+
+            //Error check
+            printf("Pointer is not set\n");
+
+            //makefile checks
+            if(strcmp(argv[i], "Makefile") == 0) {
+                Make++;
+            } //Fuck you for segfaulting like a lil bitch
+
+            else if(strcmp(argv[i], "makefile") == 0) {
+                Make++;
+            }
+
+                //If it is not a makefile, it is another
+            else {
+                Other++;
+            }
+        }
 
 
-            if(strcmp(point + 1, "c") == 0) {
+            //if arg has a '.', checks the char right after
+
+//            if(strcmp(argv[i]+strlen(argv[i] - 1), cFile) == 0) {
+             if(point+1 == 'c') {
                 cSource+=1;
             }
 
-            if (strcmp(point + 1, "h") == 0) {
+//            if (strcmp(argv[i]+strlen(argv[i] - 1), hFile) == 0) {
+            if(point+1 == 'h') {
                 cHead++;
             }
 
-            if(strcmp(point + 1, "o") == 0) {
+//            if(strcmp(argv[i]+strlen(argv[i] - 1), oFile) == 0) {
+            if(point + 1 == 'o') {
                 Obj++;
             }
 
-            if(strcmp(argv[i], "Makefile") == 0) {
+        //redundant code
+/*            if(strcmp(argv[i], "Makefile") == 0) {
                 Make++;
             }
 
-            if(strcmp(argv[i], "makefile") == 0) {
+            else if(strcmp(argv[i], "makefile") == 0) {
                 Make++;
             }
+
             else {
                 Other++;
+            }*/
+
         }
             //printf(strrchr('.', ));
-    }
 
+    //Counters
     printf("C Source files: %i \n", cSource);
     printf("C Header files: %i \n", cHead);
     printf("Objects: %i \n", Obj);
